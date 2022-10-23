13484368b46f356edf271fe8a3bea2c4519c7a63
Milán Kormány <kormany.1993@gmail.com>
2022-10-23 10:00:06 +0200

    Update settings.php

--- Website Code/settings.php
+++ Website Code/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-define("API_KEY","8f686190-33eb-4d35-920b-b2345230e504");
+define("API_KEY","");
 define("API_HOST","https://api.sandbox.transferwise.tech");
 define("COUNTRY_DATA_PATH","./data/data.csv");
  
